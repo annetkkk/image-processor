bb72ce34cca9c13ea712e7efea8c59992842c982
Anna Pozdeeva <anna_pozdeeva1804@mail.ru>
2026-05-02 10:49:04 +0300

    Add arg_parser.h for image processing argument handling

---
+++ arg_parser.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct FilterArgs {
+    std::string filter_name;
+    std::vector<std::string> filter_args;
+};
+
+struct ImageProcessorArgs {
+    std::string input_file;
+    std::string output_file;
+    std::vector<FilterArgs> filters;
+};
+
+ImageProcessorArgs ExtractImageProcessorArgs(int argc, char** argv);
